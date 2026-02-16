bc2d4166de2b90054227744b329dcf8fba7dbed6
kashikaryash <kashikaryash@gmail.com>
2026-02-16 22:04:17 +0530

    Initial commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8082/api/employees';
+const API_BASE_URL = 'industrious-friendship-production.up.railway.app/api/employees';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
